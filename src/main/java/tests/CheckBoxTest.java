1666ae435ad30ea0df4b7af0e3ebb87c619d644e
tuvinh96 <vinhqt170796@gmail.com>
2026-05-15 18:23:42 +0700

    commit homework

--- src/main/java/tests/CheckBoxTest.java
+++ src/main/java/tests/CheckBoxTest.java
@@ -1,14 +1,49 @@
 package tests;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class CheckBoxTest extends TestCase{
 	@Test
-	public void verifyCheckboxSuccessfully() {
+	public void verifyCheckBoxCorrectly(){
 		testBase.webDriver.navigate().to("https://demoqa.com/checkbox");
-		testBase.webDriver.findElement(By.xpath("//span[@class='rct-title' and text()='Home']/ancestor::span[@class='rct-text']//span[contains(@class,'rct-checkbox')]")).click();
-//		testBase.webDriver.findElement(tglExpand).click();
+		WebElement cbHome = testBase.webDriver.findElement(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Home\"]"));
+		String cbHomeStatus = cbHome.getAttribute("class");
+//		System.out.println(cbHomeStatus);
+		Assert.assertFalse(cbHomeStatus.contains("checkbox-checked"));
+		cbHome.click();
+		List<WebElement> elements = testBase.webDriver.findElements(By.xpath("//span[@class=\"text-success\"]"));
+		String actualText = "You have selected :";
+		String expectedText = "You have selected : home desktop notes commands documents workspace react angular veu office public private classified general downloads wordFile excelFile";
+		for (WebElement webElement : elements) {
+			String selectedItem = webElement.getText();
+			actualText = actualText + " " + selectedItem;
+
+		}
+//		System.out.println(actualText);
+//		System.out.println(expectedText);
+		Assert.assertTrue(actualText.equals(expectedText));
+		WebElement expandHome = testBase.webDriver.findElement(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Home\"]/preceding-sibling::span[1]"));
+		expandHome.click();
+		WebDriverWait wait = new WebDriverWait(testBase.webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Desktop\"]")));
+		WebElement cbDesktop = testBase.webDriver.findElement(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Desktop\"]"));
+		WebElement cbDocuments = testBase.webDriver.findElement(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Documents\"]"));
+		WebElement cbDownloads = testBase.webDriver.findElement(By.xpath("//*[contains(@class,\"rc-tree-checkbox\") and @aria-label=\"Select Downloads\"]"));
+		String cbDesktopStatus = cbDesktop.getAttribute("class");
+		String cbDocumentsStatus = cbDocuments.getAttribute("class");
+		String cbDownloadStatus = cbDownloads.getAttribute("class");
+		Assert.assertTrue(cbDesktopStatus.contains("checkbox-checked"));
+		Assert.assertTrue(cbDocumentsStatus.contains("checkbox-checked"));
+		Assert.assertTrue(cbDownloadStatus.contains("checkbox-checked"));
 	}
 
 }
--- src/main/java/tests/TextBoxTest.java
+++ src/main/java/tests/TextBoxTest.java
@@ -1,44 +1,60 @@
 package tests;
 
+import java.time.Duration;
+
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import pages.TextBoxPage;
 
 public class TextBoxTest extends TestCase {
-	@Test
-	public void submitDataSuccessfully() {
-		System.setProperty("webdriver.chrome.driver",
-				"F:\\Study\\01_AutomationClass202604\\03_Practices\\02Projects\\Git\\DemoQA\\DemoQA\\driver\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		WebDriver webDriver = new ChromeDriver(options);
-		webDriver.get("https://demoqa.com/text-box");
-		webDriver.manage().window().maximize();
-		webDriver.findElement(By.id("userName")).sendKeys("test");
-		webDriver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
-		webDriver.findElement(By.id("currentAddress")).sendKeys("test address");
-		webDriver.findElement(By.id("permanentAddress")).sendKeys("test perma address");
-		webDriver.findElement(By.id("submit")).click();
+//	@Test
+//	public void submitDataSuccessfully() {
+//		System.setProperty("webdriver.chrome.driver",
+//				"F:\\Study\\01_AutomationClass202604\\03_Practices\\02Projects\\Git\\DemoQA\\DemoQA\\driver\\chromedriver.exe");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--remote-allow-origins=*");
+//		WebDriver webDriver = new ChromeDriver(options);
+//		webDriver.get("https://demoqa.com/text-box");
+//		webDriver.manage().window().maximize();
+//		webDriver.findElement(By.id("userName")).sendKeys("test");
+//		webDriver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
+//		webDriver.findElement(By.id("currentAddress")).sendKeys("test address");
+//		webDriver.findElement(By.id("permanentAddress")).sendKeys("test perma address");
+//		webDriver.findElement(By.id("submit")).click();
+//
+//	}
 
-	}
-	
 	@Test
-	public void submitDataFail() {
-		System.setProperty("webdriver.chrome.driver",
-				"F:\\Study\\01_AutomationClass202604\\03_Practices\\02Projects\\Git\\DemoQA\\DemoQA\\driver\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		WebDriver webDriver = new ChromeDriver(options);
-		webDriver.get("https://demoqa.com/text-box");
-		webDriver.manage().window().maximize();
-		webDriver.findElement(By.id("userName")).sendKeys("Quang Vinh");
-		webDriver.findElement(By.id("userEmail")).sendKeys("testgmail.com");
-		webDriver.findElement(By.id("currentAddress")).sendKeys("Khe Hoi, Thuong Tin");
-		webDriver.findElement(By.id("permanentAddress")).sendKeys("Thuong Tin, Ha Noi");
-		webDriver.findElement(By.id("submit")).click();
-
+	public void emailFormatIsWrong() throws InterruptedException {
+//		System.setProperty("webdriver.chrome.driver",
+//				"F:\\Study\\01_AutomationClass202604\\03_Practices\\02Projects\\Git\\DemoQA\\DemoQA\\driver\\chromedriver.exe");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--remote-allow-origins=*");
+//		WebDriver webDriver = new ChromeDriver(options);
+//		webDriver.get("https://demoqa.com/text-box");
+//		webDriver.manage().window().maximize();
+		testBase.webDriver.navigate().to("https://demoqa.com/text-box");
+		testBase.webDriver.findElement(By.id("userName")).sendKeys("Quang Vinh");
+		testBase.webDriver.findElement(By.id("userEmail")).sendKeys("testgmail.com");
+		testBase.webDriver.findElement(By.id("currentAddress")).sendKeys("Khe Hoi, Thuong Tin");
+		testBase.webDriver.findElement(By.id("permanentAddress")).sendKeys("Thuong Tin, Ha Noi");
+		testBase.webDriver.findElement(By.id("submit")).click();
+		Thread.sleep(200);
+//		WebDriverWait wait = new WebDriverWait(testBase.webDriver, Duration.ofSeconds(5));
+//		wait.until(ExpectedConditions.attributeContains(By.id("userEmail"), "class", "field-error"));
+		WebElement txtEmail = testBase.webDriver.findElement(By.id("userEmail"));
+		String borderEmail = txtEmail.getCssValue("border-top-color");
+//		System.out.println(borderEmail);
+		Assert.assertTrue(borderEmail.contains("255, 0, 0"));
+		Assert.assertTrue(testBase.webDriver.findElements(By.xpath("//p[@class=\"mb-1\"]")).isEmpty());
 	}
-	
+
 }
